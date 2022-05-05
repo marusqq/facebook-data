991f1c61e623806dcdbd12aec5a3b0bb1d863147
Marius Pozniakovas <marius.pozniakovas.contractor@draeger.com>
2022-05-05 14:22:47 +0300

    updated Parser

--- Parser.py
+++ Parser.py
@@ -4,11 +4,22 @@ import util
 
 class Parser:
     def __init__(self, path):
-        self.path = util.check_if_facebook_data_path_is_okay(path)
-        self.split_messenger_groups()
+        self.inbox_path = util.check_if_facebook_data_path_is_okay(path)
+        self.messenger_group_archives = self.set_messenger_groups()
+        self.unrar_groups()
+        self.read_messenger_groups()
 
-    def split_messenger_groups(self):
-        print(self.path)
+    def set_messenger_groups(self):
+        return [x for x in self.inbox_path.iterdir() if x.is_file()]
+
+    def read_messenger_groups(self):
         pass
 
+    def parse_group_messages(self, group):
+        pass
+
+    def unrar_groups(self):
+        for archive in self.messenger_group_archives:
+            util.unrar_file(archive)
+        pass
 
--- util.py
+++ util.py
@@ -1,8 +1,32 @@
+from pathlib import Path
+import rarfile
+import platform
 
-def check_if_facebook_data_path_is_okay(path) -> str:
+
+def check_if_facebook_data_path_is_okay(path) -> Path:
     """
+    Checks inputted_path for path/inbox dir,
+    then checks for path/inbox having subdirectories
+    Returns path on success, raises SystemExit exception on failure
     :param path: inputted path
     :return: path: checked path
+    :raises: SystemExit: on failure
     """
 
-    return path
+    _path = Path(path)
+
+    inbox = _path / 'inbox'
+    if not inbox.is_dir():
+        raise SystemExit(f'Bad path for facebook data in directory: "{path}"')
+
+    dirs = [x for x in _path.iterdir() if x.is_dir()]
+    if not len(dirs):
+        raise SystemExit(f'No directories found in directory: "{inbox}"')
+
+    return inbox
+
+
+def unrar_file(filepath):
+    rar_file = rarfile.RarFile(filepath)
+    parent_path = filepath.parent
+    rar_file.extractall(parent_path / 'extracted' / filepath.stem)
